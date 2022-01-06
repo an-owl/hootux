736befa24f9a450eca19eaf3e007d1c2327df4d9
an_owl <an_owl@protonmail.com>
2022-01-06 17:32:12 +1300

    split into lib

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+#![no_std]
+#![no_main]
+#![feature(const_mut_refs)]
+
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+pub mod vga_text;
+pub mod serial;
+
+pub trait Testable{
+    fn run (&self);
+}
+
+impl<T> Testable for T
+    where T: Fn()
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[PASSED]");
+    }
+}
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]){
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run()
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+pub fn test_panic(info: &core::panic::PanicInfo) -> ! {
+    serial_println!("[FAILED]");
+    serial_println!("Error: {}", info);
+    exit_qemu(QemuExitCode::Failed);
+    loop {}
+}
+
+#[cfg(test)]
+#[no_mangle]
+pub extern "C" fn _start() -> !{
+
+    test_main();
+    loop{}
+}
+
+
+#[cfg(test)]
+#[panic_handler]
+pub fn panic(info: &core::panic::PanicInfo) -> ! {
+    test_panic(info)
+}
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode{
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode){
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,7 @@
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_text;
-mod serial;
+use owl_os::*;
 
 #[no_mangle]
 pub extern "C" fn _start() -> !{
@@ -19,19 +18,6 @@ pub extern "C" fn _start() -> !{
     panic!("Almost fell through");
 }
 
-pub fn exit_qemu(exit_code: QemuExitCode){
-    use x86_64::instructions::port::Port;
-
-    unsafe {
-        let mut port = Port::new(0xf4);
-        port.write(exit_code as u32);
-    }
-}
-
-
-
-
-
 #[cfg(not(test))]
 #[panic_handler]
 fn panic_handler(info: &core::panic::PanicInfo) -> !{
@@ -39,15 +25,6 @@ fn panic_handler(info: &core::panic::PanicInfo) -> !{
     println!("{}", info);
 
     loop{}
-}
-
-#[cfg(test)]
-#[panic_handler]
-fn test_panic(info: &core::panic::PanicInfo) -> ! {
-    serial_println!("[FAILED]");
-    serial_println!("Error: {}", info);
-    exit_qemu(QemuExitCode::Failed);
-    loop {}
 }
 
 #[cfg(test)]
@@ -59,28 +36,8 @@ fn test_runner(tests: &[&dyn Testable]){
     exit_qemu(QemuExitCode::Success);
 }
 
-
-
-
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-#[repr(u32)]
-pub enum QemuExitCode{
-    Success = 0x10,
-    Failed = 0x11,
-}
-
-pub trait Testable{
-    fn run (&self);
-}
-
-impl<T> Testable for T
-    where T: Fn()
-{
-    fn run(&self) {
-        serial_print!("{}...\t", core::any::type_name::<T>());
-        self();
-        serial_println!("[PASSED]");
-    }
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    test_panic(info)
 }
