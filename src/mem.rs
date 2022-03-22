f5d3c05c7f2bf5d26897738c16d5dcac3bf0d8a5
an_owl <an_owl@protonmail.com>
2022-03-22 15:34:52 +1300

    added docs

--- src/mem.rs
+++ src/mem.rs
@@ -2,6 +2,8 @@ use x86_64::{structures::paging::PageTable,VirtAddr,PhysAddr};
 use x86_64::structures::paging::{FrameAllocator, OffsetPageTable, PhysFrame, Size4KiB};
 use bootloader::boot_info::{MemoryRegion, MemoryRegionKind};
 
+
+/// A FrameAllocator that returns usable frames from the bootloader's memory map.
 pub struct BootInfoFrameAllocator{
     memory_map: &'static [MemoryRegion],
     next: usize,
@@ -9,12 +11,20 @@ pub struct BootInfoFrameAllocator{
 
 impl BootInfoFrameAllocator{
 
+    /// Create a FrameAllocator from the passed memory map.
+    ///
+    /// This function is unsafe because the caller must guarantee that the passed
+    /// memory map is valid. The main requirement is that all frames that are marked
+    /// as `USABLE` in it are really unused.
     pub unsafe fn init(memory_map: &'static [MemoryRegion]) -> Self {
         BootInfoFrameAllocator {
             memory_map,
             next: 0,
         }
     }
+
+
+    /// Returns an iterator over the usable frames specified in the memory map.
     fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
         let regions = self.memory_map.iter();
         let usable_regions = regions
@@ -37,6 +47,13 @@ unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator{
     }
 }
 
+
+/// Returns a mutable reference to the active level 4 table.
+///
+/// This function is unsafe because the caller must guarantee that the
+/// complete physical memory is mapped to virtual memory at the passed
+/// `physical_memory_offset`. Also, this function must be only called once
+/// to avoid aliasing `&mut` references (which is undefined behavior).
 unsafe fn active_l4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable{
     use x86_64::registers::control::Cr3;
     let (l4,_) = Cr3::read();
@@ -49,6 +66,12 @@ unsafe fn active_l4_table(physical_memory_offset: VirtAddr) -> &'static mut Page
 
 }
 
+/// Initialize a new OffsetPageTable.
+///
+/// This function is unsafe because the caller must guarantee that the
+/// complete physical memory is mapped to virtual memory at the passed
+/// `physical_memory_offset`. Also, this function must be only called once
+/// to avoid aliasing `&mut` references (which is undefined behavior).
 pub unsafe fn init(offset: VirtAddr) -> OffsetPageTable<'static> {
     let l4 = active_l4_table(offset);
     OffsetPageTable::new(l4,offset)
