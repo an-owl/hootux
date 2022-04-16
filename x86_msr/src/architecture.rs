c3b18d6d4d46ab119188abb8b09546dce4f88070
an_owl <an_owl@protonmail.com>
2022-04-16 15:00:14 +1200

    improved docs and cleaned with "Rustfmt"

--- x86_msr/src/architecture.rs
+++ x86_msr/src/architecture.rs
@@ -1,54 +1,56 @@
 //! This module contains registers marked as Architectural by the [Intel software developers manual volume 4](https://cdrdv2.intel.com/v1/dl/getContent/671098)
-//! all MSR names in this module the their prefix "ia-32_" omitted and written in camel case
+//! all MSR names in this module the their prefix "IA32_" omitted and written in camel case
 //! i.e "IA32_TIME_STAMP_COUNTER" becomes "TimeStampCounter"
 //!
-//! MSR addresses can be found in their
+//! Any modifications to an instance of `impl MsrFlags` must be manually written back the the register
 //!
 //! Not all MSRs are currently implemented
 
-
-
+use crate::MsrReadWrite::Reserved;
 use crate::*;
 use bitflags::bitflags;
-use crate::MsrReadWrite::Reserved;
 
 /// IA_32_TIME_STAMP_COUNTER see <https://www.intel.com/content/dam/develop/public/us/en/documents/335592-sdm-vol-4.pdf>
 /// for more info
-pub struct TimeStampCounter{}
+pub struct TimeStampCounter {}
 #[derive(Copy, Clone)]
-pub struct TimeStampCounterValue{
+
+/// Contains a value for TimeStampCounterValue
+///
+/// All values for this register are valid
+pub struct TimeStampCounterValue {
     pub value: u64, //pub because it doesnt really need protection
 }
 
-impl Msr<TimeStampCounterValue> for TimeStampCounter{
+impl Msr<TimeStampCounterValue> for TimeStampCounter {
     const MSR_ADDR: u32 = 0x10;
-    #[cold]
     fn availability() -> MsrAvailability {
         return if cpuid_lookup_bit(CpuidRegister::edx, 1, 0, 4) {
             MsrAvailability::new(MsrReadWrite::Write)
         } else {
             MsrAvailability::new(MsrReadWrite::Reserved)
-        }
+        };
     }
 }
 
-impl MsrFlags for TimeStampCounterValue{
+impl MsrFlags for TimeStampCounterValue {
     fn bits(&self) -> (u32, u32) {
         let low = self.value as u32;
         let high = (self.value >> 32) as u32;
 
-        return (high,low)
+        return (high, low);
     }
 
     fn new(high: u32, low: u32) -> Self {
-        let total = Self::bitfield(high,low);
-        Self{value: total}
+        let total = Self::bitfield(high, low);
+        Self { value: total }
     }
 }
-/// This for whatever reason does not work
-pub struct SpecCtrl{}
+/// This for whatever reason does not work.
+/// I would however like to know what it does work on
+pub struct SpecCtrl {}
 
-bitflags!{
+bitflags! {
     /// This for whatever reason does not work
     pub struct SpecCtrlFlags: u64 {
         const INDIRECT_BRANCH_RESTRICTED_SPECULATION =      1 << 0;
@@ -57,19 +59,21 @@ bitflags!{
     }
 }
 
-impl MsrFlags for SpecCtrlFlags{
+impl MsrFlags for SpecCtrlFlags {
     fn bits(&self) -> (u32, u32) {
         let low = self.bits as u32;
-        let high = (self.bits >> 32 ) as u32;
-        (high,low)
+        let high = (self.bits >> 32) as u32;
+        (high, low)
     }
 
     fn new(high: u32, low: u32) -> Self {
-        Self{bits: Self::bitfield(high,low)}
+        Self {
+            bits: Self::bitfield(high, low),
+        }
     }
 }
 
-impl Msr<SpecCtrlFlags> for SpecCtrl{
+impl Msr<SpecCtrlFlags> for SpecCtrl {
     const MSR_ADDR: u32 = 0x48;
 
     fn availability() -> MsrAvailability {
@@ -83,7 +87,7 @@ impl Msr<SpecCtrlFlags> for SpecCtrl{
             bits.0[1] = MsrReadWrite::Write;
         }
 
-        if cpuid_lookup_bit(CpuidRegister::edx,7,0,31){
+        if cpuid_lookup_bit(CpuidRegister::edx, 7, 0, 31) {
             bits.0[2] = MsrReadWrite::Write;
         }
 
@@ -91,9 +95,11 @@ impl Msr<SpecCtrlFlags> for SpecCtrl{
     }
 }
 
-pub struct ApicBase{}
+/// This register contains information about the Apic including its base physical address
+pub struct ApicBase {}
 
 bitflags! {
+    /// see [ApicBase]
     pub struct ApicBaseData: u64{
         const BSP_FLAG =            1 << 8;
         const X2APIC_ENABLE_MODE =  1 << 10;
@@ -101,41 +107,49 @@ bitflags! {
     }
 }
 
-impl ApicBaseData{
-    pub fn get_apic_base_addr(&self) -> u64{
+impl ApicBaseData {
+    /// This returns the apic base address
+    pub fn get_apic_base_addr(&self) -> u64 {
         self.bits & !Self::all().bits
     }
 
-    pub fn set_apic_base_addr(&mut self, base_addr: u64){
+    /// This returns the apic base address
+    pub fn set_apic_base_addr(&mut self, base_addr: u64) {
         assert!(base_addr < *MAXPHYADDR_MASK);
         self.bits &= !*MAXPHYADDR_MASK << 12; // clear bits in address range
         self.bits &= base_addr << 12;
     }
 }
 
-impl MsrFlags for ApicBaseData{
+impl MsrFlags for ApicBaseData {
     fn bits(&self) -> (u32, u32) {
         Self::split_bits(self.bits)
     }
 
     fn new(high: u32, low: u32) -> Self {
-        Self{bits: Self::bitfield(high,low)}
+        Self {
+            bits: Self::bitfield(high, low),
+        }
     }
 }
 
-impl Msr<ApicBaseData> for ApicBase{
+impl Msr<ApicBaseData> for ApicBase {
     const MSR_ADDR: u32 = 0x1b;
 
+    /// While `BSP_FLAG` is flagged as write that's probably not a good idea
     fn availability() -> MsrAvailability {
         let cpuid = raw_cpuid::CpuId::new();
-        let feature = cpuid.get_feature_info().expect("unable to get feature info");
-
+        let feature = cpuid
+            .get_feature_info()
+            .expect("unable to get feature info");
 
         let mut bits = MsrAvailability::new(MsrReadWrite::Reserved);
-        if ((feature.family_id() == 6) && (feature.model_id() >= 1)) || feature.family_id() > 6{
+        if ((feature.family_id() == 6) && (feature.model_id() >= 1)) || feature.family_id() > 6 {
             bits.0[8] = MsrReadWrite::Write;
-            bits.0[11..*MAXPHYADDR as usize-1].fill_with(||{MsrReadWrite::Write});
-            if((feature.family_id() == 6) && (feature.model_id() >= 0x1a)) || feature.family_id() > 6 {
+            bits.0[11..*MAXPHYADDR as usize - 1].fill_with(|| MsrReadWrite::Write);
+            if ((feature.family_id() == 6) && (feature.model_id() >= 0x1a))
+                || feature.family_id() > 6
+            {
                 bits.0[10] = MsrReadWrite::Write;
             }
         }
@@ -143,4 +157,3 @@ impl Msr<ApicBaseData> for ApicBase{
         bits
     }
 }
-
