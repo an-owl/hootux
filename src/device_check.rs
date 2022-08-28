65930a40534305206576dca997f1de4c1569f12f
an_owl <an_owl@protonmail.com>
2022-08-28 12:56:51 +1200

    changed MaybeExists to use impl DeviceCheck

--- src/device_check.rs
+++ src/device_check.rs
@@ -1,29 +1,31 @@
 use core::fmt::Formatter;
 
 /// Enables simple hardware checking
-pub(crate) struct MaybeExists<T,const CHECK: CheckType>{
-    inner: T
+/// Checks type U for the presence of T
+pub(crate) struct MaybeExists<T,U:DeviceCheck>{
+    inner: T,
+    check: U
 }
 
-impl<T,const CHECK: CheckType> MaybeExists<T, CHECK> {
+impl<T,U:DeviceCheck> MaybeExists<T,U> {
     pub fn try_fetch(&self) -> Option<&T>{
-        if CHECK.exists() {
+        if U::exists() {
             Some(&self.inner)
         } else { None }
     }
 
     pub fn try_fetch_mut(&mut self) -> Option<&mut T> {
-        if CHECK.exists() {
+        if U::exists() {
             Some(&mut self.inner)
         } else { None }
     }
 }
 
-impl<T,const CHECK: CheckType> core::fmt::Debug for MaybeExists<T,CHECK>
+impl<T,U:DeviceCheck> core::fmt::Debug for MaybeExists<T,U>
     where T: core::fmt::Debug
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        if CHECK.exists() {
+        if U::exists() {
             write!(f,"{:?}",self.inner)
         } else {
             write!(f, "Not Present")
@@ -31,11 +33,11 @@ impl<T,const CHECK: CheckType> core::fmt::Debug for MaybeExists<T,CHECK>
     }
 }
 
-impl<T,const CHECK: CheckType> core::fmt::Display for MaybeExists<T,CHECK>
+impl<T,U:DeviceCheck> core::fmt::Display for MaybeExists<T,U>
     where T: core::fmt::Debug
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
-        if CHECK.exists() {
+        if U::exists() {
             write!(f,"{:?}",self.inner)
         } else {
             write!(f, "Not Present")
@@ -43,24 +45,6 @@ impl<T,const CHECK: CheckType> core::fmt::Display for MaybeExists<T,CHECK>
     }
 }
 
-#[derive(Eq, PartialEq,Debug)]
-#[non_exhaustive]
-pub(crate) enum CheckType{
-    ApicThermalInterrupt,
-    ApicPerformanceInterrupt,
-}
-
-impl CheckType {
-    fn exists(&self) -> bool {
-        return match self {
-            CheckType::ApicThermalInterrupt => {
-                raw_cpuid::CpuId::new().get_feature_info().unwrap().has_acpi()
-            }
-            CheckType::ApicPerformanceInterrupt => {
-                return if let Some(_) = raw_cpuid::CpuId::new().get_performance_monitoring_info() {
-                    true
-                } else { false }
-            }
-        }
-    }
+pub(crate) trait DeviceCheck{
+    fn exists() -> bool;
 }
