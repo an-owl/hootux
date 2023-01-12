99b3e0ec5c3622ce6da8ce7c246e82b69095a675
an_owl <an_owl@protonmail.com>
2023-01-12 13:50:09 +1300

    fixed some bugs

--- kernel/src/kernel_structures/mod.rs
+++ kernel/src/kernel_structures/mod.rs
@@ -3,3 +3,10 @@ pub mod static_protected;
 
 pub use mutex::{Mutex, MutexGuard};
 pub use static_protected::{KernelStatic, UnlockedStatic};
+
+/// Struct to provide !Send, !Sync traits
+#[derive(Default,Copy, Clone)]
+#[repr(transparent)]
+pub struct PhantomUnsend {
+    _t: core::marker::PhantomData<*mut ()>
+}
