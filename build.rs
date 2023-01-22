ecaa1f8fc4c84655c1ffa32effaf1abb4ab52267
an_owl <an_owl@protonmail.com>
2023-01-22 14:38:03 +1300

    Changed output directory

--- build.rs
+++ build.rs
@@ -1,24 +1,26 @@
-
 fn main() {
-    let out_dir = std::env::var_os("OUT_DIR").unwrap().into_string().unwrap();
+    let out_dir = "./target/".to_string();
     let kernel = if let Some(k) = std::env::var_os("CARGO_BIN_FILE_HOOTUX_hootux") {
         k.into_string().unwrap()
     } else {
         eprintln!("CARGO_BIN_FILE_HOOTUX_hootux not found\n");
-        for (k,v) in std::env::vars() {
+        for (k, v) in std::env::vars() {
             eprintln!("{k} = {v}");
-        };
+        }
         std::process::exit(1);
     };
 
     let uefi_build_out = out_dir.clone() + "uefi.img";
-    bootloader::UefiBoot::new(std::path::Path::new(&kernel)).create_disk_image(uefi_build_out.as_ref()).unwrap();
+    bootloader::UefiBoot::new(std::path::Path::new(&kernel))
+        .create_disk_image(uefi_build_out.as_ref())
+        .unwrap();
 
     let bios = out_dir.clone() + "bios.img";
-    bootloader::BiosBoot::new(std::path::Path::new(&kernel)).create_disk_image(bios.as_ref()).unwrap();
+    bootloader::BiosBoot::new(std::path::Path::new(&kernel))
+        .create_disk_image(bios.as_ref())
+        .unwrap();
 
     println!("cargo:rustc-env=UEFI_PATH={uefi_build_out}");
     println!("cargo:rustc-env=BIOS_PATH={bios}");
     println!("cargo:rustc-env=KERNEL_BIN={kernel}");
-
 }
