7d4e92a83ac6695f78256b349aab0a474decbfc2
an_owl <32559552+an-owl@users.noreply.github.com>
2023-11-22 17:37:07 +1300

    Added Write Once Read Many type

--- kernel/src/kernel_structures/mod.rs
+++ kernel/src/kernel_structures/mod.rs
@@ -3,10 +3,13 @@ pub mod static_protected;
 
 pub use mutex::{Mutex, MutexGuard};
 pub use static_protected::{KernelStatic, UnlockedStatic};
+pub(crate) use worm::Worm;
+
+mod worm;
 
 /// Struct to provide !Send, !Sync traits
-#[derive(Default,Copy, Clone)]
+#[derive(Default, Copy, Clone)]
 #[repr(transparent)]
 pub struct PhantomUnsend {
-    _t: core::marker::PhantomData<*mut ()>
+    _t: core::marker::PhantomData<*mut ()>,
 }
---
+++ kernel/src/kernel_structures/worm.rs
@@ -0,0 +1,68 @@
+/// Write Once Read Many
+///
+/// Worm is designed to be used to store system statics that need to be initialized at runtime.
+#[derive(Default, Debug)]
+pub(crate) struct Worm<T> {
+    inner: core::cell::UnsafeCell<Option<T>>,
+}
+
+impl<T> Worm<T> {
+    pub(crate) const fn new() -> Self {
+        Self {
+            inner: core::cell::UnsafeCell::new(None),
+        }
+    }
+    /// Sets the inner value
+    ///
+    /// # Panics
+    ///
+    /// This fn will panic if this fn has already been called on `self`.
+    ///
+    /// # Safety
+    ///
+    /// This is racy it should not be called after multiprocessing has been started.
+    #[cfg_attr(debug_assertions, track_caller)]
+    pub(crate) unsafe fn write(&self, v: T) {
+        let t = unsafe { &mut *self.inner.get() };
+        if t.is_none() {
+            Some(v)
+        } else {
+            panic!("Worm already initialized");
+        };
+    }
+
+    /// Returns a reference to the inner data
+    ///
+    /// # Panics
+    ///
+    /// This fn will panic if [Self::write] has not been called on `self`
+    #[cfg_attr(debug_assertions, track_caller)]
+    pub(crate) fn read(&self) -> &T {
+        // SAFETY: This is safe because the returned value is not mutably referenced
+        let t = unsafe { &*self.inner.get() };
+        let r = t.as_ref();
+        if t.is_some() {
+            r.unwrap()
+        } else {
+            panic!("Worm not initialized");
+        }
+    }
+
+    /// Returns whether or not [Self::write] has been called
+    pub(crate) fn is_set(&self) -> bool {
+        // SAFETY: see Self::read
+        unsafe { &*self.inner.get() }.is_some()
+    }
+}
+
+/// See [Worm::read] for info
+impl<T> core::ops::Deref for Worm<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        self.read()
+    }
+}
+
+unsafe impl<T> Sync for Worm<T> where T: Sync {}
+unsafe impl<T> Send for Worm<T> where T: Send {}
