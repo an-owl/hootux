aa0476380b0eff3e26b659e826e80b6d3e772054
an_owl <32559552+an-owl@users.noreply.github.com>
2023-12-05 14:23:03 +1300

    Added unsafe box, so I don't need to mutability alias things

--- kernel/src/kernel_structures/mod.rs
+++ kernel/src/kernel_structures/mod.rs
@@ -1,11 +1,12 @@
 pub mod mutex;
 pub mod static_protected;
+mod unsafe_box;
+mod worm;
 
 pub use mutex::{Mutex, MutexGuard};
 pub use static_protected::{KernelStatic, UnlockedStatic};
+pub(crate) use unsafe_box::UnsafeBox;
 pub(crate) use worm::Worm;
-
-mod worm;
 
 /// Struct to provide !Send, !Sync traits
 #[derive(Default, Copy, Clone)]
---
+++ kernel/src/kernel_structures/unsafe_box.rs
@@ -0,0 +1,68 @@
+use core::fmt::{Debug, Formatter};
+
+/// A box to contain a memory allocation without explicitly referencing memory
+/// The purpose is to allow multiple references to the contained value without explicitly aliasing
+/// the contained memory.
+///  
+/// ## Dropping
+///
+/// This will not drop contained values, however the contained memory will be freed.
+/// If a struct contains references to an UnsafeBox the box should be placed in the struct lower
+/// than anything pointing to the contained data, this is because struct fields are dropped in the
+/// order they are declared.
+/// ```
+/// struct Foo {
+///     dropped_first: &'static u8, // references the data owned by `dropped_second`
+///     dropped_second: UnsafeBox<u8>
+/// }
+/// ```
+pub(crate) struct UnsafeBox<T, A: alloc::alloc::Allocator = alloc::alloc::Global> {
+    _pin: core::marker::PhantomData<T>,
+    alloc: A,
+    ptr: *mut T,
+}
+
+impl<T, A: alloc::alloc::Allocator> UnsafeBox<T, A> {
+    pub fn new(alloc: A) -> Self {
+        let ptr = alloc
+            .allocate(alloc::alloc::Layout::new::<T>())
+            .expect("System ran out of memory")
+            .as_ptr()
+            .cast();
+        Self {
+            _pin: Default::default(),
+            alloc,
+            ptr,
+        }
+    }
+
+    /// Returns a pointer to the contained value.
+    ///
+    /// # Safety
+    ///
+    /// Normal raw pointer safety rules apply.
+    /// The caller must ensure that `self` is dropped after all references are dropped
+    pub unsafe fn ptr(&self) -> *mut T {
+        self.ptr
+    }
+}
+
+impl<T, A: alloc::alloc::Allocator> Drop for UnsafeBox<T, A> {
+    fn drop(&mut self) {
+        unsafe {
+            self.alloc.deallocate(
+                core::ptr::NonNull::new(self.ptr.cast()).unwrap(),
+                alloc::alloc::Layout::new::<T>(),
+            )
+        }
+    }
+}
+
+impl<T, A: alloc::alloc::Allocator> Debug for UnsafeBox<T, A> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> core::fmt::Result {
+        let mut d = f.debug_struct(core::any::type_name::<Self>());
+        d.field("ptr", &alloc::format!("{:#x}", self.ptr as usize));
+
+        d.finish()
+    }
+}
