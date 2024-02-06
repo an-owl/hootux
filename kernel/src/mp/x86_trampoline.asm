76e650fd08c3f6c8419d70ba2c9a5dfd6b4eb3cf
an_owl <32559552+an-owl@users.noreply.github.com>
2024-02-06 13:42:39 +1300

    Fixed all the linker problems

--- kernel/src/mp/x86_trampoline.asm
+++ kernel/src/mp/x86_trampoline.asm
@@ -1,27 +1,33 @@
- segment .hootux_ap_trampoline
+%define RQ_PAT 1
+%define RQ_ROOT_PAGE_TABLE 2
+%define RQ_LONG_MODE_STACK 3
+%define RQ_RUST_ENTRY_POINT 4
+%define RQ_TLS 5
 
- global _trampoline
- global _bsp_xfer_sem
-     bits 16
-     align 4096
- _trampoline:
+%define gdt_ptr _trampoline_data + 0
+%define rx _trampoline_data + 80
+%define tx _trampoline_data + 88
+segment .hootux_ap_trampoline
+
+global _trampoline
+global _bsp_xfer_sem
+global _initial_gdt
+    bits 16
+    align 4096
+_trampoline:
+
     cli
     cld
-    jmp get_gdt
-_bsp_xfer_sem:
-    rx: dq 0,
-    tx: dw 0,
+
+; lgdt & ljump need to be resolved manually (until i figure out why the bootloader wont co-operate)
 get_gdt:
     ;fetch GDT address
     ;GDT address will be written here before AP is started
-    xor edi,edi
-    xchg dword edi,[rx]
-    
-    lgdt [edi]
+    lgdt [cs:0x100]
     mov eax,cr0
     or eax,1
     mov cr0,eax
-    jmp 8:set_stack
+    jmp 8:0x20 ; set protected mode
     
     bits 32
     align 32
@@ -29,44 +35,97 @@ set_stack:
     mov ax,16
     mov ds,ax
     mov ss,ax ; temporary stack for init
-    mov dword [tx],1
-    xor edi,edi
-spin_for_stack:
-    lock cmpxchg dword [rx],edi
-    pause
-    je spin_for_stack
-    mov esp,edi
+
+    mov esp,4096 ; set stack pointer
     
 set_longmode:
+    mov esi, RQ_PAT ; get PAT values
+    call request_32 ; calls are absolute, but near within the segment
+    mov ecx,0x277
+    ; ignore esi, read result manually
+    mov dword eax,[rx] ; do these need to be DS:[..]?
+    mov dword edx,[rx+4]
+    wrmsr ; set PAT
+
+    mov esi,RQ_ROOT_PAGE_TABLE
+    call request_32 ; request top level page table
+    mov cr3,edi
+
     mov ecx,0xC0000080
     rdmsr
     or eax,1 << 8
-    wrmsr ; efer.lme is set
-    
-    mov esi,2 ; get PAT values
-    call request
-    mov ecx,0x277
-    mov dword eax,[rx]
-    mov dword edx,[rx+4]
-    wrmsr ; set PAT
-    
-    mov esi,3
-    call request ; request top level page table
-    mov dword eax,[rx]
-    mov cr3,eax
-    
-    mov esi,4
-    call request ; request address for jumping to ap_startup
+    wrmsr ; now in IA-3e compatibility mode
 
+    mov esi,RQ_LONG_MODE_STACK
+    call request_32 ; ignore result, move to rsp after jmp to long mode
 
-    ;takes esi is arg 
+    jmp 24:init_longmode
+
+    ;takes esi is arg
     ;result returned in edi
-request: 
+    ;clobbers ebx
+request_32:
     mov dword [tx],esi
-    xor edx,edx
-rq_spin:
-    lock cmpxchg dword [rx],edx
+    xor ebx,ebx
+rq_spin_32:
+    ; tx will be set to 0 by bsp when data is ready
+    lock cmpxchg dword [tx],ebx
     pause
-    je rq_spin,
-    ; tx will be set to 0 by bsp
+    je rq_spin_32,
+    lfence
+    mov dword edi,[rx]
     ret
+
+    bits 64
+init_longmode:
+    mov qword rsi,[rx]
+    mov ax,16
+    mov ss,ax
+    mov ds,ax
+
+    mov esi,3
+    call request_64
+    mov rsp,rdi
+
+    mov esi,RQ_TLS ; requests and loads the thread local segment
+    call request_64
+    mov dword eax,[rel rx]
+    mov dword edx, [rel rx+4]
+    mov ecx,0xc0000100 ; uses fs_base MSR to avoid needing to use GDT
+    wrmsr
+
+    mov esi,4
+    call request_64 ; request address for jumping to ap_startup
+
+    jmp rdi
+
+    ;takes esi is arg
+    ;result returned in rdi
+    ;clobbers ebx
+request_64:
+    mov dword [tx],esi
+    xor ebx,ebx
+rq_spin_64:
+    ; tx will be set to 0 by bsp
+    lock cmpxchg dword [tx],ebx
+    pause
+    je rq_spin_64,
+    lfence
+    mov qword rdi,[rx]
+    ret
+
+    align 8
+_trampoline_data: ; trick linker into thinking this is code
+    nop ; use this as magic number [90, 0f, 0b, cc]
+    ud2
+    int3
+
+;_gdt_ptr:
+;    len dw 64,
+;    gdt_ptr dd 0,
+;; written into by BSP
+;_initial_gdt:
+;    gdt times 9 dq 0
+;_bsp_xfer_sem:
+;    rx: dq 0,
+;    tx: dw 0,
