6b9a12a4f72c2845aec50c1cdfab352140b266ff
an_owl <32559552+an-owl@users.noreply.github.com>
2024-06-14 14:27:43 +1200

    Added SingleArc type for mutable Arc

--- kernel/src/util/mod.rs
+++ kernel/src/util/mod.rs
@@ -2,11 +2,13 @@ pub mod mutex;
 pub mod static_protected;
 mod unsafe_box;
 mod worm;
+mod single_arc;
 
 pub use mutex::Mutex;
 pub use static_protected::{KernelStatic, UnlockedStatic};
 pub(crate) use unsafe_box::UnsafeBox;
 pub(crate) use worm::Worm;
+pub use single_arc::*;
 
 /// Struct to provide !Send, !Sync traits
 #[derive(Default, Copy, Clone)]
---
+++ kernel/src/util/single_arc.rs
@@ -0,0 +1,155 @@
+//! Similar to a [alloc::sync::Arc] allows only one strong reference which can be mutable.
+//!
+//! Internally uses a box so the data can be freed and memory recovered before all [Weak] are dropped.
+
+use alloc::boxed::Box;
+
+/// Provides a shared reference to some data. Unlike [alloc::sync::Arc] This allows mutable access.
+/// However, any data may only have one SingleArc pointing to it. When this is dropped the data is
+/// dropped and the memory is freed.
+pub struct SingleArc<T: ?Sized> {
+    // SAFETY: We can freely dereference this. While self exists so does *inner
+    inner: *mut SingleArcInner<T>,
+}
+
+unsafe impl<T> Send for SingleArc<T> where T: Send{}
+unsafe impl<T> Sync for SingleArc<T> where T: Sync{}
+
+impl<T: ?Sized> SingleArc<T> {
+    pub fn new(data: Box<T>) -> Self {
+        let inner = Box::leak(
+            Box::new(
+                SingleArcInner{
+                    weak_count: atomic::Atomic::new(0),
+                    data: core::cell::UnsafeCell::new(Some(data))
+                }
+            )
+        );
+        Self {
+            inner,
+        }
+    }
+
+    pub fn downgrade(&self) -> Weak<T> {
+        unsafe { &*self.inner }.weak_count.fetch_add(1,atomic::Ordering::Acquire);
+        Weak {
+            inner: Some(self.inner),
+        }
+    }
+
+    pub fn take(self) -> Box<T> {
+        let inner = unsafe { &*self.inner };
+        // SAFETY: SingleArc owns this data and can do what it wants with it
+        unsafe { &mut *inner.data.get() }.take().unwrap()
+    }
+}
+
+impl<T: ?Sized> core::ops::Deref for SingleArc<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        // SAFETY: Only Self can access the data and only one may exist which references the data
+        unsafe { (&*(&*self.inner).data.get()).as_ref().unwrap() }
+    }
+}
+
+impl<T: ?Sized> core::ops::DerefMut for SingleArc<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        // SAFETY: Only Self can access the data and only one may exist which references the data
+        unsafe { (&mut *(&mut *self.inner).data.get()).as_mut().unwrap() }
+    }
+}
+
+impl<T: ?Sized> Drop for SingleArc<T> {
+    fn drop(&mut self) {
+        unsafe {
+            // SAFETY: self.inner is synchronized and owned by self, so we can access it freely
+            let inner = &mut *self.inner;
+            // SAFETY: We own this data
+            let _ = (&mut *inner.data.get()).take();
+            if inner.weak_count.load(atomic::Ordering::Relaxed) == 0 {
+                // SAFETY: this is constructed using Box::new and leaked, we just undo it here.
+                // We also check that this is not aliased
+                let _ = Box::from_raw(inner);
+            }
+        }
+    }
+}
+
+impl<T: ?Sized> From<Box<T>> for SingleArc<T> {
+    fn from(value: Box<T>) -> Self {
+        let inner = Box::leak(Box::new(
+            SingleArcInner{
+                weak_count: atomic::Atomic::new(0),
+                data: core::cell::UnsafeCell::new(Some(value))
+            }
+        ));
+
+        Self {
+            inner,
+        }
+    }
+}
+
+/// Weak reference to the inner data.
+pub struct Weak<T: ?Sized> {
+    inner: Option<*mut SingleArcInner<T>>,
+}
+
+
+impl<T: ?Sized> Default for Weak<T> {
+    fn default() -> Self {
+        Self{
+            inner: None
+        }
+    }
+}
+
+impl<T: ?Sized> Drop for Weak<T> {
+    fn drop(&mut self) {
+
+        unsafe {
+            if let Some(inner) = self.inner {
+                // SAFETY: self.inner is synchronized and owned by self, so we can access it freely
+                let inner = &mut *inner;
+                inner.weak_count.fetch_sub(1, atomic::Ordering::Release);
+                if inner.weak_count.load(atomic::Ordering::Relaxed) == 0 && (&*inner.data.get()).is_none() {
+                    // SAFETY: this is constructed using Box::new and leaked, we just undo it here
+                    // We also check that this is not aliased
+                    let _ = Box::from_raw(inner);
+                }
+            }
+        };
+    }
+}
+
+impl<T: ?Sized> Weak<T> {
+    pub fn get(&self) -> Option<*mut T> {
+        // Absolute abomination
+        // SAFETY: All data in here is guaranteed to be accessible.
+        // SAFETY: This *could* cause a use after free. But its up to the caller to actually
+        // validate that the returned pointer is valid.
+        let t = unsafe { &mut *(&**self.inner.as_ref()?).data.get() };
+        if let Some(t) = t {
+            let data: &mut T = &mut *t;
+            Some(data)
+        } else {
+            None
+        }
+    }
+
+    /// Drops any inner references freeing memory is possible.
+    pub fn clear(&mut self) {
+        core::mem::take(self);
+    }
+
+    /// Sets self to point to the data contained within `strong`.
+    pub fn set(&mut self, strong: &SingleArc<T>){
+        *self = strong.downgrade()
+    }
+}
+
+struct SingleArcInner<T: ?Sized> {
+    weak_count: atomic::Atomic<usize>,
+    data: core::cell::UnsafeCell<Option<Box<T>>>
+}
