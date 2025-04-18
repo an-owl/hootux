05f78d45b0664efa8fc57a7a52e4d5835fcba9d7
an_owl <32559552+an-owl@users.noreply.github.com>
2025-04-18 14:21:21 +1200

    Added an address to locate the BSP stack

--- lib/libboot/src/variables.rs
+++ lib/libboot/src/variables.rs
@@ -10,3 +10,8 @@ pub const PHYS_OFFSET_ADDR: usize = {
 
 // todo make configurable
 pub const STACK_SIZE: usize = 0x100000;
+
+/// Unused for EFI
+/// Indicates the top of the stack. Note that on x86 targets the indicated address should not be mapped.
+// l4 entry #510 last 4K page is stack guard.
+pub const STACK_ADDR: usize = 0xFFFFFFFFF000;
