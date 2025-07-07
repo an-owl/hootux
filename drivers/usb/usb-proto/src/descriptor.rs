85a68fef0a720db5c2d9feae4309752f53ad18a2
an_owl <32559552+an-owl@users.noreply.github.com>
2025-07-08 11:32:00 +1200

    Added library for USB configuration types

---
+++ drivers/usb/usb-proto/src/descriptor.rs
@@ -0,0 +1,298 @@
+use crate::DescriptorType;
+use crate::descriptor::sealed::Sealed;
+use bitfield::bitfield;
+use bitflags::bitflags;
+use num_enum::{IntoPrimitive, TryFromPrimitive};
+
+/// Contains information about the device capabilities.
+///
+/// Returned by the [super::RequestCode::GetDescriptor] when the device descriptor is set to
+/// request the device descriptor.
+#[repr(C, packed)]
+pub struct DeviceDescriptor {
+    pub length: u8,
+    pub descriptor_type: u8,
+    pub bcd_usb: BcdUsb,
+    pub device_class: u8,
+    pub device_sub_class: u8,
+    pub device_protocol: u8,
+    /// Describes the maximum packet size for a configuration transaction.
+    /// Only 8,16,32,64 are valid values for this field.
+    /// When a device is high-speed the only valid value for this field is `64`
+    pub max_packet_size_0: u8,
+    pub vendor_id: u16,
+    pub product_id: u16,
+    pub bcd_device: u16,
+    pub manufacturer: u8,
+    pub product: u8,
+    pub serial_number: u8,
+    /// Indicates the number of [ConfigurationDescriptor]'s that are available.
+    pub num_configurations: u8,
+}
+
+impl DeviceDescriptor {
+    const fn class(&self) -> [u8; 3] {
+        [
+            self.device_class,
+            self.device_sub_class,
+            self.device_protocol,
+        ]
+    }
+}
+
+/// Contains a USB revision number as a binary coded decimal.
+///
+/// The BCD format ix `0xMMmr` Where `MM` is the major version, `m` is the minor version and `r` is the revision.
+/// Version 2.3.6 is encoded `0x0236`.
+#[repr(transparent)]
+#[derive(Copy, Clone, Debug)]
+pub struct BcdUsb(pub u16);
+
+impl core::fmt::Display for BcdUsb {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        let bytes = self.0.to_le_bytes();
+        f.write_fmt(format_args!("{}.{}", bytes[0], bytes[1] & 0xf0))?;
+        if bytes[1] & 0x0f != 0 {
+            f.write_fmt(format_args!(".{}", bytes[1] & 0x0f))?
+        };
+        Ok(())
+    }
+}
+
+impl core::fmt::LowerHex for BcdUsb {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl core::fmt::UpperHex for BcdUsb {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+/// Contains information about configurations when the configured speed is not the current speed.
+/// This can ba fetched from the device using [super::CtlTransfer::get_descriptor] where using [super::DescriptorType::DeviceQualifier]
+#[repr(C, packed)]
+pub struct DeviceQualifier {
+    pub length: u8,
+    pub descriptor_type: u8,
+    pub bcd_usb: BcdUsb,
+    pub device_class: u8,
+    pub device_sub_class: u8,
+    pub device_protocol: u8,
+    pub max_packet_size_0: u8,
+    pub num_configurations: u8,
+    _reserved: u8,
+}
+
+/// Contains information about a specific device configuration.
+/// The number of possible configurations is returned by [DeviceDescriptor::num_configurations]
+///
+/// This can be fetched using [super::CtlTransfer::get_descriptor] using [super::DescriptorType::Configuration],
+///
+/// When `DescriptorType::OtherSpeedConfiguration` is used this describes a configuration when the
+/// device is using a speed which it is not currently using.
+#[repr(C, packed)]
+pub struct ConfigurationDescriptor<T: ConfigurationMarker = Normal> {
+    pub length: u8,
+    pub descriptor_type: u8,
+    /// Requesting this descriptor will also return all associated [InterfaceDescriptor]'s and [EndpointDescriptor]'s,
+    /// this field indicates the total size of all these fields.
+    pub total_length: u16,
+    /// Indicates the number of interfaces implemented by this configuration.
+    pub num_interfaces: u8,
+    /// Value given to [super::CtlTransfer::set_configuration] to assign this configuration to the device.
+    pub configuration_value: u8,
+    /// Indicates the string index of this descriptor.
+    pub configuration_index: u8,
+    pub attributes: ConfigurationAttruibutes,
+    pub max_power: u8,
+}
+
+macro_rules! protected_trait {
+    (@config: $ty:ty) => {
+        impl ConfigurationMarker for $ty {}
+        impl Sealed for $ty {}
+    };
+    (@resolv: $ty:ty -> $out:expr) => {
+        impl RequestableDescriptor for $ty {
+            const DESCRIPTOR_TYPE: DescriptorType = $out;
+        }
+    };
+}
+
+pub trait ConfigurationMarker: Sealed {}
+pub struct Normal;
+protected_trait!(@config: Normal);
+pub struct AlternateSpeed;
+protected_trait!(@config: AlternateSpeed);
+
+bitflags! {
+    #[repr(transparent)]
+    #[derive(Copy, Clone, Debug)]
+    struct ConfigurationAttruibutes: u8 {
+        const SELF_POWERED = 1 << 6;
+        const REMOTE_WAKEUP = 1 << 5;
+    }
+}
+
+mod sealed {
+    pub trait Sealed {}
+}
+
+pub trait RequestableDescriptor: Sealed {
+    const DESCRIPTOR_TYPE: super::DescriptorType;
+}
+
+protected_trait!(@resolv: DeviceDescriptor -> DescriptorType::Device);
+protected_trait!(@resolv: DeviceQualifier -> DescriptorType::DeviceQualifier);
+protected_trait!(@resolv: ConfigurationDescriptor -> DescriptorType::Configuration);
+protected_trait!(@resolv: ConfigurationDescriptor<AlternateSpeed> -> DescriptorType::OtherSpeedConfiguration);
+
+#[repr(C, packed)]
+pub struct InterfaceDescriptor {
+    pub length: u8,
+    pub descriptor_type: u8,
+    pub interface_number: u8,
+    pub alternate_setting: u8,
+    pub num_endpoints: u8,
+    pub interface_class: u8,
+    pub interface_sub_class: u8,
+    pub interface_protocol: u8,
+    pub interface_index: u8,
+}
+
+impl InterfaceDescriptor {
+    fn class(&self) -> [u8; 3] {
+        [
+            self.interface_class,
+            self.interface_sub_class,
+            self.interface_protocol,
+        ]
+    }
+}
+
+#[repr(C, packed)]
+pub struct EndpointDescriptor {
+    pub length: u8,
+    pub descriptor_type: u8,
+    pub endpoint_address: EndpointAddress,
+    pub attributes: EndpointAttributes,
+    pub max_packet_size: MaxPacketSize,
+    /// Polling rate for periodic devices.
+    ///
+    /// For high/full speed isochronous endpoints and high-speed interrupt endpoints this value
+    /// must be `1..=16`. and is an exponential (2^num).
+    /// A value of 4 indicates a period of 4.
+    ///
+    /// For full/low speed interrupt endpoints this value may be between `1..=255`
+    ///
+    /// For full/low speed devices this number must be multiplied
+    /// by 8 to convert frame-times into microframes.
+    ///
+    /// For high speed bulk OUT endpoints this indicates the maximum NAK rate of the endpoint.
+    /// A value of `0` indicates this endpoint will never return NAK.
+    /// Other values indicate at most 1 NAK each `interval` of microframes.
+    pub interval: u8,
+}
+
+bitfield! {
+    #[repr(transparent)]
+    #[derive(Copy, Clone)]
+    struct EndpointAddress(u8);
+    impl Debug;
+    endpoint, _: 6,0;
+    in_endpoint, _: 7, 4;
+}
+
+bitfield! {
+    #[repr(transparent)]
+    #[derive(Copy, Clone)]
+    struct EndpointAttributes(u8);
+    impl Debug;
+
+    /// Indicates the endpoint type, [Self::endpoint_synchronisation] and [Self::endpoint_usage]
+    /// are only valid if this is [EndpointTransferType::Isochronous].
+    from EndpointTransferType, transfer_type,_: 1,0;
+    from EndpointSynchronisationType, endpoint_synchronisation,_:  3,2;
+    from EndpointUsageType, endpoint_usage, _: 5,4
+}
+
+#[derive(TryFromPrimitive, Copy, Clone)]
+enum EndpointTransferType {
+    Control = 0,
+    Isochronous,
+    Bulk,
+    Interrupt,
+}
+
+impl From<u8> for EndpointTransferType {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => EndpointTransferType::Control,
+            1 => EndpointTransferType::Isochronous,
+            2 => EndpointTransferType::Bulk,
+            3 => EndpointTransferType::Interrupt,
+            _ => panic!("invalid endpoint transfer type {value}"),
+        }
+    }
+}
+
+impl From<EndpointTransferType> for u8 {
+    fn from(value: EndpointTransferType) -> Self {
+        value as u8
+    }
+}
+
+pub enum EndpointSynchronisationType {
+    NoSynchronisation = 0,
+    /// An asynchronous-isochronous endpoint derives its sample rate from an external source,
+    /// such as a crystal and is not synchronised to the USB bus.
+    /// Explicit feedback information must be provided so the device driver can adjust data rates.
+    Asynchronous = 1,
+    /// Adaptive endpoints may sink or send data at any rate within their operating range.
+    /// The consumer must provide feedback to the producer to adjust the data rate.
+    Adaptive = 2,
+    /// An adaptive-isochronous endpoint derives its sample rate from SOF, the sample rate is
+    /// deterministic and periodic.
+    Synchronous = 3,
+}
+
+impl From<u8> for EndpointSynchronisationType {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => EndpointSynchronisationType::NoSynchronisation,
+            1 => EndpointSynchronisationType::Adaptive,
+            2 => EndpointSynchronisationType::Synchronous,
+            3 => EndpointSynchronisationType::Adaptive,
+            _ => panic!("invalid endpoint synchronisation {value}"),
+        }
+    }
+}
+
+pub enum EndpointUsageType {
+    Data = 0,
+    Feedback = 1,
+    ImplicitFeedbackData = 2,
+}
+
+impl From<u8> for EndpointUsageType {
+    fn from(value: u8) -> Self {
+        match value {
+            0 => EndpointUsageType::Data,
+            1 => EndpointUsageType::Feedback,
+            2 => EndpointUsageType::ImplicitFeedbackData,
+            _ => panic!("invalid endpoint usage type {value}"),
+        }
+    }
+}
+
+bitfield! {
+    struct MaxPacketSize(u8);
+    impl Debug;
+    max_packet_size, _: 10,0;
+    /// Indicates the number of extra transaction opportunities per microframe.
+    /// `self.isochronous_mult() + 1` indicates number of possible transactions per microframe.
+    isochronus_mult, _: 12,11;
+}
---
+++ drivers/usb/usb-proto/src/lib.rs
@@ -0,0 +1,377 @@
+#![no_std]
+//! This crate defines commands and return values used by the usb protocol.
+
+mod descriptor;
+
+use bitfield::bitfield;
+
+/// This represents a configuration request to a USB device. The operation of this is defined in
+/// section 9.2 & 9.3 of the USB bus specification 2.0.
+///
+/// Configuration transactions may require transmitting data in either direction,
+/// if so then the transaction where `CtlTransfer` was sent must be followed by more transactions using the appropriate PID.
+/// On a device-to-host transaction the device may not send more then [Self::data_len] bytes, however it may send less.
+/// On a host-to-device transaction if the host will send [Self::data_len] bytes.
+/// When more bytes are sent to the device than was expected the results are undefined.
+///
+/// # Errors
+///
+/// When an illegal request is made to a device it must respond with "Stall".
+#[repr(C, packed)]
+pub struct CtlTransfer {
+    request_type: RequestHeader,
+    request: u8,
+    value: u16,
+    index: u16,
+    length: u16,
+}
+
+impl CtlTransfer {
+    fn new(
+        recipient: Recipient,
+        request_type: RequestType,
+        will_receive_data: bool,
+        request: u8,
+        value: u16,
+        index: u16,
+        length: u16,
+    ) -> Self {
+        let mut header = RequestHeader(0);
+        header.recipent(recipient);
+        header.target_type(request_type);
+        header.data_direction(will_receive_data);
+        Self {
+            request_type: header,
+            request,
+            value,
+            index,
+            length,
+        }
+    }
+
+    fn data_len(&self) -> usize {
+        self.length as usize
+    }
+}
+
+impl CtlTransfer {
+    /// Clears the feature specified by `feature_selector`.
+    /// `feature_selector` should be cast from [FeatureDescriptor] where possible.
+    ///
+    /// `recipient` may not be [Recipient::Other]
+    // fixme double check that
+    ///
+    /// Attempting to clear a feature which cannot be cleared, does not exist or references an
+    /// endpoint that does not exist will return an error
+    ///
+    /// Note: [FeatureDescriptor::TestMode] cannot be cleared
+    // todo separate into endpoint & non-endpoint forms
+    pub const fn clear_feature(recipient: Recipient, feature_selector: u16) -> Self {
+        let mut header = RequestHeader(0);
+        header.recipent(recipient);
+        Self {
+            request_type: header,
+            request: RequestCode::ClearFeature as u8,
+            value: feature_selector,
+            index: 0,
+            length: 0,
+        }
+    }
+
+    /// Returns the current configuration of the device.
+    /// If the configuration is `0` then the device is not configured.
+    pub fn get_configuration() -> Self {
+        let mut header = RequestHeader(0);
+        header.data_direction(true);
+
+        Self {
+            request_type: header,
+            request: RequestCode::GetConfiguration as u8,
+            value: 0,
+            index: 0,
+            length: 1, // Why is this one? It returns 2 bytes. Is it one transaction?
+        }
+    }
+
+    /// Returns the specified descriptor if it exists.
+    fn get_descriptor_private(
+        descriptor_type: DescriptorType,
+        descriptor_index: u8,
+        lang: u16,
+        len: u16,
+    ) -> Self {
+        let mut header = RequestHeader(0);
+        header.data_direction(true);
+        Self {
+            request_type: header,
+            request: RequestCode::GetDescriptor as u8,
+            value: u16::from_le_bytes([descriptor_index, descriptor_type as u8]),
+            index: lang,
+            length: len,
+        }
+    }
+
+    /// This will construct a request that will request the descriptor `D`.
+    /// When either [descriptor::DeviceDescriptor] or [descriptor::DeviceQualifier] is requested
+    /// `index` will be silently driven to 0.
+    ///
+    /// When requesting [descriptor::ConfigurationDescriptor] the device will attempt to send all
+    /// associated [descriptor::InterfaceDescriptor]'s and [descriptor::EndpointDescriptor]'s.
+    /// To determine the buffer length required to fetch all associated data the driver should
+    /// request the required `ConfigurationDescriptor` with `len` set to `None`, the returned
+    /// descriptor will contain the total length of the configuration.
+    ///
+    /// If `len` is `None` it will be set to `core::mem::size_of::<D>()`.
+    pub fn get_descriptor<D: descriptor::RequestableDescriptor>(
+        mut index: u8,
+        len: Option<u16>,
+    ) -> Self {
+        if core::any::TypeId::of::<D>() == core::any::TypeId::of::<descriptor::DeviceDescriptor>()
+            || core::any::TypeId::of::<D>()
+                == core::any::TypeId::of::<descriptor::DeviceQualifier>()
+        {
+            index = 0;
+        }
+
+        let mut header = RequestHeader(0);
+        header.data_direction(true);
+        Self {
+            request_type: header,
+            request: RequestCode::GetDescriptor as u8,
+            value: u16::from_le_bytes([index, D::DESCRIPTOR_TYPE as u8]),
+            index: 0,
+            length: len.unwrap_or(size_of::<D>() as u16),
+        }
+    }
+
+    /// Some devices have configurations with interfaces that have mutually exclusive settings.
+    /// This request will return the currently selected alternate setting.
+    pub fn get_interface(interface: u16) -> Self {
+        let mut header = RequestHeader(0);
+        header.data_direction(true);
+        Self {
+            request_type: header,
+            request: RequestCode::GetInterface as u8,
+            value: 0,
+            index: interface,
+            length: 1,
+        }
+    }
+
+    /// This returns a status value indicating hte state of the recipient.
+    ///
+    /// When `recipient` is [Recipient::Device] this the return value contains two bitflags.
+    /// Bit 0 is set when the device is self powered. Bit 1 is set when the device supports remote wakeup.
+    /// Remote wakeup will be clear unless the feature was explicitly enabled by the host.
+    ///
+    /// When `recipient` is [Recipient::Interface] the returned status is reserved.
+    ///
+    /// When `recipient` is [Recipient::Endpoint] bit 0 indicates whether the endpoint is currently halted.
+    ///
+    /// # Panics
+    ///
+    /// This fn will panic if `recipient` is [Recipient::Other] or if
+    pub fn get_status(recipient: Recipient, interface: u16) -> Self {
+        let mut header = RequestHeader(0);
+        header.data_direction(true);
+        header.recipent(recipient);
+
+        assert_ne!(recipient, Recipient::Other);
+        if recipient == Recipient::Device && interface != 0 {
+            panic!("interface must be `0` when recipient is Device");
+        }
+
+        Self {
+            request_type: header,
+            request: RequestCode::GetStatus as u8,
+            value: 0,
+            index: interface,
+            length: 2,
+        }
+    }
+
+    /// Assigns a new address to the device. If the device has not been assigned an address and the
+    /// address is non-zero this will update it to the "address" state. All successive transactions
+    /// must be to the new address.
+    ///
+    /// # Panics
+    ///
+    /// This fn will panic if `address` is greater than `127`.
+    pub fn set_address(address: u8) -> Self {
+        assert!(address < 128u8);
+        Self {
+            request_type: RequestHeader(0),
+            request: RequestCode::SetAddress as u8,
+            value: address as u16,
+            index: 0,
+            length: 0,
+        }
+    }
+
+    /// This request will set the device to use the configuration with the index `configuration_value`.
+    /// If `configuration_value` is `0` the device will be deconfigured and placed in the address state.
+    pub fn set_configuration(configuration_value: u8) -> Self {
+        Self {
+            request_type: RequestHeader(0),
+            request: RequestCode::SetConfiguration as u8,
+            value: configuration_value as u16,
+            index: 0,
+            length: 0,
+        }
+    }
+
+    // no set_descriptor
+
+    /// Sets `feature_selector` for the recipient. The caller muse ensure that `feature_selector`
+    /// is appropriate for `recipient`.
+    ///
+    /// If the [FeatureDescriptor::TestMode] feature is selected `recipient` must be [Recipient::Device] and `target`
+    /// must contain the test mode for the device.
+    ///
+    /// When either [Recipient::Interface] or [Recipient::Endpoint] is selected `target` describes
+    /// the interface or endpoint.
+    pub fn set_feature(recipient: Recipient, feature_selector: u16, target: u16) -> Self {
+        let mut header = RequestHeader(0);
+        header.recipent(recipient);
+        assert_ne!(recipient, Recipient::Other);
+
+        Self {
+            request_type: header,
+            request: RequestCode::SetFeature as u8,
+            value: feature_selector,
+            index: target,
+            length: 0,
+        }
+    }
+
+    /// This allows setting the device to an alternate interface. This request amy only be sent if
+    /// the device supports alternate interfaces.
+    /// This is not used to change the set of configured interfaces. see [Self::set_configuration]
+    /// for initial configuration.
+    pub fn set_interface(interface: u16, alternate: u16) -> Self {
+        let mut header = RequestHeader(0);
+        header.recipent(Recipient::Interface);
+        Self {
+            request_type: header,
+            request: RequestCode::SetInterface as u8,
+            value: alternate,
+            index: interface,
+            length: 0,
+        }
+    }
+
+    /// Some isochronous transfer sizes may vary on a per-frame basis according to a pattern.
+    /// The host and endpoint must agree on which frame the pattern begins.
+    /// The frame where the pattern begins will be returned to the host.
+    pub fn synch_frame(endpoint: u16) -> Self {
+        let mut header = RequestHeader(0);
+        header.data_direction(true);
+        header.recipent(Recipient::Interface);
+        Self {
+            request_type: header,
+            request: RequestCode::SynchFrame as u8,
+            value: 0,
+            index: endpoint,
+            length: 2,
+        }
+    }
+}
+
+bitfield! {
+    #[derive(Copy, Clone)]
+    struct RequestHeader(u8);
+    impl Debug;
+
+    from Recipient, _, recipent: 4,0;
+    from RequestType, _, target_type: 6,5;
+    from DataDirection, _, data_direction: 7;
+
+}
+
+#[derive(Copy, Clone, Debug)]
+enum DataDirection {
+    HostToDevice = 0,
+    DeviceToHost = 1,
+}
+
+impl From<DataDirection> for bool {
+    fn from(value: DataDirection) -> Self {
+        value as u8 != 0
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default, Eq, PartialEq)]
+pub enum RequestType {
+    #[default]
+    Standard = 0,
+    Interface = 1,
+    Vendor = 2,
+}
+
+impl From<RequestType> for u8 {
+    fn from(value: RequestType) -> Self {
+        value as u8
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum Recipient {
+    Device = 0,
+    Interface = 1,
+    Endpoint = 2,
+    Other = 3,
+}
+
+impl From<Recipient> for u8 {
+    fn from(value: Recipient) -> Self {
+        value as u8
+    }
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum RequestCode {
+    GetStatus = 0,
+    ClearFeature = 1,
+    SetFeature = 3,
+    SetAddress = 5,
+    GetDescriptor = 6,
+    SetDescriptor = 7,
+    GetConfiguration = 8,
+    SetConfiguration = 9,
+    GetInterface = 10,
+    SetInterface = 11,
+    SynchFrame = 12,
+}
+
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum DescriptorType {
+    Device = 1,
+    Configuration = 2,
+    String = 3,
+    Interface = 4,
+    Endpoint = 5,
+    DeviceQualifier = 6,
+    OtherSpeedConfiguration = 7,
+    InterfacePower = 8,
+}
+
+#[non_exhaustive]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum FeatureDescriptor {
+    EndpointHalt = 0,
+    DeviceRemoteWakeup = 1,
+    TestMode = 2,
+}
+
+/// Test modes defined by the USB specification 2.0
+///
+/// Values of `0xc0..=0xff` are are allowed for vendor specific test modes.
+#[non_exhaustive]
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+pub enum TestMode {
+    TestJ = 1,
+    TestK,
+    TestSe0Nak,
+    TestPacket,
+    TestForceEnable,
+}
