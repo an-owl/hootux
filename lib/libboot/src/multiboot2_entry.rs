94b633663973f8f20069fcc1ae06cf0cda2747d2
an_owl <32559552+an-owl@users.noreply.github.com>
2025-10-20 13:42:39 +1300

    CHanged multiboot2 PM enty to use statically allocated memory

--- lib/libboot/src/multiboot2_entry.rs
+++ lib/libboot/src/multiboot2_entry.rs
@@ -1172,7 +1172,7 @@ pub(crate) mod pm {
                     used_boundary: self.curr_state,
                     // SAFETY: This is safe, this is defined in the global_asm block where all variables are dword sized
                     // This symbol will never be written to once
-                    low_boundary: ALLOC_COUNTER as u64,
+                    low_boundary: 0,
                 }
             }
         }
@@ -1198,17 +1198,11 @@ pub(crate) mod pm {
         }
     }
 
-    unsafe extern "C" {
-
-        /// Indicates how many pages were freed when switching to the kernel.
-        ///
-        /// ## SAFETY
-        ///
-        /// This symbol must not be written to after entering long-mode,
-        /// and no references to it may escape libhatcher.
-        #[link_name = "alloc_counter"]
-        static ALLOC_COUNTER: u32;
-    }
+    #[repr(align(4096))]
+    #[allow(dead_code)] // This gets used in asm, but rustc doesn't know that.
+    struct InitialStack([u8; 0x3000]);
+    #[unsafe(link_section = ".text.hatcher.entry.multiboot2.pm_arena")]
+    static mut PB_ARENA: InitialStack = InitialStack([0; 0x3000]);
 
     global_asm!(
         "
@@ -1220,30 +1214,12 @@ pub(crate) mod pm {
 
         .equ entry_pointer_offset, hatcher_multiboot2_pm_entry-.L_get_eip
 
-        .equ initial_gdt_offset,initial_gdt-hatcher_multiboot2_pm_entry
-
-        .equ mem_map_base_offset, mem_map_base - hatcher_multiboot2_pm_entry
-
-        .equ elf_headers_base_offset, elf_headers_base - hatcher_multiboot2_pm_entry
-
-        .equ alloc_counter_offset, alloc_counter - hatcher_multiboot2_pm_entry
+        .equ initial_gdt_offset, initial_gdt-hatcher_multiboot2_pm_entry
 
         initial_gdt:
             .8byte 0
             .8byte {KNL_CODE_SEG_BITS}
             .8byte {KNL_DATA_SEG_BITS}
-
-        alloc_counter:
-            .4byte 0
-
-        mem_map_base:
-            .4byte 0
-
-        elf_headers_base:
-            .4byte 0
-
-        initial_stack:
-            .fill 32,4,0
 
         /// This handles switching the CPU to long mode, and handing over to rust code.
         /// ebx will point to the MBI structure until rust code is called.
@@ -1260,32 +1236,22 @@ pub(crate) mod pm {
             pop ebp
 
             lea ebp,[ebp+entry_pointer_offset] // locates the address of hatcher_multiboot2_pm_entry if we are relocated
+            mov esp,ebp
 
             mov ecx,{CR0_INITIAL}
             mov cr0,ecx
             mov ecx,{CR4_INITIAL}
             mov cr4,ecx
-            mov esp,ebp // points to the top of the stack
 
-            mov ecx,{MEMORY_MAP_TAG}
-
-            call __hatcher_mb2pm_tag_finder
-
-
-        // We need to allocate memory.
-        // To do this we need to parse memory map, and compare it with the ELF headers and MBI address
-        // Here we locate the tags for the ELF headers and memory map
-            lea edi,[ebp-mem_map_base_offset]
-            mov [edi],eax
-
-            // locate elf headers
-            mov ecx,{ELF_HEADERS_TAG}
-            call __hatcher_mb2pm_tag_finder
-            lea edi,[ebp-elf_headers_base_offset]
-            mov [edi],eax
+            lea eax,[hatcher_multiboot2_pm_entry]
+            sub eax,ebp
+            lea ecx,{INITIAL_STACK}+(0x1000*3)
+            add ecx,eax
+            mov esp,ecx // last page in INITIAL_STACK
+        _l3_setup:
         .L_setup_l3_table:
-            call __hatcher_mb2pm_alloc_mem
-            mov edi,eax
+            mov edi,esp
+            sub edi,(0x1000*3) // Start address of initial stack
             mov ecx,{GIANT_PAGE_BITS}
             xor esi,esi
             xor edx,edx
@@ -1299,27 +1265,16 @@ pub(crate) mod pm {
             cmp esi,{PAGE}
             jc .L_setup_l3_inner // While page offset < {PAGE} continue
 
-
+        setup_l4:
         .L_setup_l4_table:
             push edi
-            call __hatcher_mb2pm_alloc_mem
-            mov ecx,4096
-            mov edi,eax
-            push eax
-            xor eax,eax
-            rep stosb
-            pop eax
-            pop edi
-            or edi,{PAGE_BITS}&0xf // set lower bits
-            mov [eax],edi // set first entry
-            mov cr3,eax
+            add edi,0x1000 // InitialStack + 0x1000
+            pop esi
+            or esi,{PAGE_BITS}&0xf // set lower bits
+            mov [edi],esi // set first entry
+            mov cr3,edi
 
-            /// allocate initial stack
-            call __hatcher_mb2pm_alloc_mem
-            mov esp,eax
-            add esp,{PAGE}
-
-
+        _setup_gdt:
             // setup gdt
             sub esp,6 // 6 bytes for [2+4] [len + address]
             mov word ptr [esp],0x17          // set size for gdt, (2 entries + null entry) - 1 byte
@@ -1337,8 +1292,9 @@ pub(crate) mod pm {
             or eax,0x80000000 // 1 < 31 paging bit bit
             mov cr0,eax
             // we are now in compatibility mode
-            long_mode:
 
+            long_mode:
+            sub esp,16 // allocate stack space (aligned to 16)
             /// Calculate the entry address
             /// Compiler is having a hissy fit about doing this at build time so it's being done at runtime
             lea ecx,[hatcher_multiboot2_pm_entry] // get VMA offset (when not relocated result will be 0)
@@ -1361,235 +1317,10 @@ pub(crate) mod pm {
 
         .L_fail:
             ud2
-
-        __hatcher_mb2pm_tag_finder:
-            mov esi,8 // Start of first tag
-
-            /// Locates the tag found in `ecx`.
-            /// Returns the address of the tag in `eax`.
-
-            .L_tag_finder_inner:
-            // Here we iterate over each tag to locate the simple memory tag
-            // Locating free memory is important because the first thing we need to do is locate
-            // enough memory to allocate a stack and set up page tables to initialize long-mode
-            cmp ecx,[ebx+esi]
-            jnz .L_wrong_tag
-
-            lea eax,[ebx+esi] // eax contains the address of the tag
-            ret
-
-            .L_wrong_tag:
-            mov edx,[ebx+esi+4]
-            mov eax,8-1 // align mask
-
-            // Align up to 8
-
-            // if len & 8-1 == 0 {{
-            //     len & (8-1)
-            // }} else {{
-            //     (len & (8-1)) + 8
-            // }}
-
-            and eax,edx
-            and edx,{ALIGN_8_UPPER_MASK} // clears lower 4 bits
-
-            push 8
-            cmp eax,0
-            cmovne eax,[esp]
-            add esp,4
-
-            add edx,eax
-            add esi,edx
-            jmp .L_tag_finder_inner
-
-
-        /// Locates and allocates a page from the memory map returned in eax
-        __hatcher_mb2pm_alloc_mem:
-
-            // frame setup
-            //      0x0: memory entry size
-            //      0x4: number of memory-entries
-            //      0x8: current memory-entry iteration
-            //      0xc: remaining page-found count
-
-            sub esp,4 * 4 // frame size, sizeof<u32>() * words
-            mov esi,[ebp - mem_map_base_offset]
-            mov edi,[ebp - elf_headers_base_offset]
-
-            lea ecx,[ebp - alloc_counter_offset]
-            mov eax,[ecx]
-            mov dword ptr [esp+0xc],eax // load alloc counter into esp+0xc
-
-            mov ecx,[esi+8] // entry_size
-            mov eax,[esi+4] // tag len
-            sub eax,16
-            xor edx,edx
-            div ecx
-            mov dword ptr [esp+4],eax // write number of mem elements to stack
-
-            add esi,16 // move esi to first mem map element
-
-            mov dword ptr [esp+8],0 // iteration is 0
-
-        /// locates the next usable element in the memory map
-        .L_alloc_mem_loop:
-
-            mov edx,[esi+4] // address upper half
-            test edx,0 // assert that this is not dma64 memory
-            jne .L_alloc_mem_loop_continue
-
-
-            // check that memory-type is usable
-            mov edx,[esi+16]
-            cmp edx,1
-            jne .L_alloc_mem_loop_continue
-
-        .L_alloc_mem_region_loop:
-            mov eax,[esi] // lower address half
-            mov ecx,[esi+8] // length lower half
-            // we dont fuck with the upper half because we cannot access it anyway
-            cmp ecx,{PAGE} // check that this is larger than a page
-            jb .L_alloc_mem_loop_continue
-            .L_alloc_mem_region_loop_restart:
-            call .L_align_up
-            push esi
-            call __hatcher_address_used_check
-            pop esi
-
-            sub dword ptr [esp+0xc],1       // decrement remaining-page count
-            setnc al // trick algo into failing this address
-            bt eax,0
-
-            // jumping here causes address to be aligned upward, and the new address to be
-            // re-checked. address_used_check adds one to the address so aligning it up checks the next page
-            // todo: optimize the shit out of this, by returning the amount of occupied space in the region in `ecx` on a fail
-            jc .L_alloc_mem_region_loop_restart
-
-            add dword ptr [ebp-alloc_counter_offset],1 // next call to __hatcher_mb2pm_alloc_mem must find an extra 'unused' frame
-
-            add esp,4*4
-            ret
-
-        /// Aligns eax up to 4096 and checks that the region still contains 4096 bytes
-        /// If it doesnt it will return to `.L_alloc_mem_loop_continue`
-
-        .L_align_up:
-            mov edx,{PAGE}-1
-
-            and edx,eax
-            cmp edx,0
-            jz .L_align_up_ret
-
-            and eax,~({PAGE}-1)
-            add eax,{PAGE}
-            jc .L_align_up_fail
-
-            .L_align_up_ret:
-            ret
-
-        /// Failure branch.
-        /// Stack frame must be clear
-        .L_align_up_fail:
-            add esp,4 // drop return address
-            jmp .L_alloc_mem_loop_continue
-
-        /// Runs to start the next loop iteration for a mem region
-        .L_alloc_mem_loop_continue:
-            // increments iteration and check if more entries are expected
-            add dword ptr [esp+8],1 // add iteration
-
-            mov eax,[esp] // fetch element size
-            add esi,eax // increment element pointer
-
-            mov eax,[esp+8]
-            mov ecx,[edi+4]
-            cmp eax,ecx // compare `curr_iteration - max_iteration`
-            ja .L_fail     // fixme check this is correct (also add fail)
-            jmp .L_alloc_mem_loop
-
-        /// Checks that the address in `eax` is not used by the loaded ELF or MBI struct.
-        ///
-        /// On completion `eax` will contain the requested address and bit 0 will indicate if this
-        /// address can be allocated. A value of `0` in bit 0 indicates success and `1` indicates failure
-        // setting addr|1 misaligns the pointer which is why it's failure
-        //
-        // frame layout
-        //      0: initial address
-        //      4: remaining ELF iterations
-        __hatcher_address_used_check:
-            sub esp,4 * 2
-            mov [esp],eax // save address
-
-            // Load MBI base into `esi`, and last byte into `edi`
-            mov esi,ebx
-            mov edi,[ebx]
-            add edi,ebx // needs to be base+len
-            sub edi,1 // needs to compare last byte
-            call .L_address_range_check
-
-            bt eax,0 // check if fail bit is set
-            jc .L_address_used_elf_ret
-
-
-            // load elf headers pointer
-            lea esi,[ebp-elf_headers_base_offset]
-            mov edx,[esi]
-
-            // store number of iterations remaining
-            mov ecx,[edx+8]
-            mov [esp+4],ecx
-            add edx,0x14 // we only load ELF64s so the header is 48bytes long, the specification fails to mention this.
-
-            .L_address_used_elf_sections_iter:
-            .L_address_used_elf_sections_iter_continue:
-            sub dword ptr [esp+4],1 // decrement to get index. (initial index is len of array)
-            jc .L_address_used_elf_ret
-
-            // we iterate in reverse order because its easier
-            mov ecx,eax // we need eax for multiplication
-            mov eax,64 // just assume size is 64 bytes, that's how long it is for ELF64, and we dont do ELF32 here.
-            push edx
-            mul dword ptr [esp+8] // returns address of next header (edx saved so we must add 4 to esp offset)
-            sub eax,64 // previously eax was nex index, decrement it by one
-            pop edx
-            xchg eax,ecx // return address to where it belongs
-
-            mov esi,[edx+ecx+0x4]
-            cmp esi,0 // determine if section is unused, skip if so
-            je .L_address_used_elf_sections_iter_continue
-
-            mov esi,[edx+ecx+0x18] // get ELF section address. We only need lower half because we're guaranteed to be loaded below 4GiB
-            mov edi,[edx+ecx+0x28] // ELF section size
-            add edi,esi
-            sub edi,1 // last byte
-
-            call .L_address_range_check
-
-            bt eax,0 // check if 0 bit is set
-            jnc .L_address_used_elf_sections_iter // next iteration
-
-        /// return from address_used_check
-        .L_address_used_elf_ret:
-
-            add esp,4 * 2
-            ret
-
-        /// Checks that the range described in `esi`:`edi` is not within the page pointed to by `eax`.
-        .L_address_range_check:
-            mov ecx,eax // copy address
-            add ecx,{PAGE}-1 // We need to check the entire frame, so we check against the last byte in the frame
-            cmp ecx,esi // sub address by mbi_base
-            jc .L_address_range_check_ret // overflows if pointer is below mbi_base.
-
-            cmp eax,edi
-            setc al // overflow indicates that address is lower than MBI end, so it must point within the MBI. Set fail bit
-            .L_address_range_check_ret:
-            ret
         ",
         CR0_INITIAL = const 0x20010001u32,
         CR4_INITIAL = const 0x00000020u32,
-        MEMORY_MAP_TAG = const 6u32,
-        ELF_HEADERS_TAG = const 9u32,
+        INITIAL_STACK = sym PB_ARENA,
         MULTIBOOT2_MAGIC = const 0x36d76289u32,
         PAGE = const 0x1000u32,
         GIANT_PAGE_BITS = const 0x83u32, // set present,page_size,writable bits
@@ -1599,7 +1330,6 @@ pub(crate) mod pm {
         KNL_DATA_SEG = const 16u16,
         EFER_BITS = const 0x900u32,
         EFER_ADDR = const 0xC000_0080u32,
-        ALIGN_8_UPPER_MASK = const !(8-1),
         KNL_DATA_SEG_BITS = const 0xcf93000000ffffu64,
         KNL_CODE_SEG_BITS = const 0xaf9b000000ffffu64,
         entry_64 = sym hatcher_entry_mb2pm
